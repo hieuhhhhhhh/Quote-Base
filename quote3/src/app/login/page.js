6b98979ae503a93c25c1e95303ca0f5493690170
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-09-13 21:09:41 -0400

    set up log in page

--- quote3/src/app/login/page.js
+++ quote3/src/app/login/page.js
@@ -1,3 +1,19 @@
+import styles from "@/styles/components/login_box.module.css";
+
 export default function Home() {
-  return <div>this is login</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <form>
+          <div className={styles.inputField}>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className={styles.inputField}>
+            <input type="password" placeholder="Password" />
+          </div>
+          <button className={styles.button}>Log In</button>
+        </form>
+      </div>
+    </div>
+  );
 }
