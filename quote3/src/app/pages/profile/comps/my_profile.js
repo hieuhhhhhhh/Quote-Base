19116d7a021cd0b397c0ee81c38acd66f22e098c
hieuhhhhhhh <hieuhaihuoc31102@gmail.com>
2024-10-05 03:25:23 -0400

    Edit profile part 1
    author: Evan B.

--- quote3/src/app/pages/profile/comps/my_profile.js
+++ quote3/src/app/pages/profile/comps/my_profile.js
@@ -1,11 +1,10 @@
-// useSelector count: 3
-
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import { getPublicInfo } from "@/lib/front_end/user_info/public_info"; // Import the function to fetch biography
+import { getPublicInfo } from "@/lib/front_end/user_info/public_info"; // Fetch biography
+import styles from "../Profile.module.css";
 
 function MyProfile() {
-  // Accessing redux:
+  // Accessing redux state
   const myUsername = useSelector((state) => state.myProfile.username);
   const myAlias = useSelector((state) => state.myProfile.alias);
   const myId = useSelector((state) => state.myProfile.myId);
@@ -13,22 +12,38 @@ function MyProfile() {
   const [bio, setBio] = useState("");
 
   useEffect(() => {
-    // fetch other data that redux not have:
+    // Fetch additional data not available in redux
     const fetchDB = async () => {
       const data = await getPublicInfo(myId);
       setBio(data.biography);
     };
 
     fetchDB();
-  }, [myId]); // re-fetch if my id changes
+  }, [myId]); // Re-fetch if myId changes
 
   return (
-    <div>
-      <h1>Profile Information</h1>
-      <div>This is your profile.</div>
-      <p>Username: {myUsername}</p>
-      <p>Alias: {myAlias}</p>
-      <p>Biography: {bio}</p> {/* Display the biography */}
+    <div className={styles.container}>
+      <div className={styles.profileContentMain}>
+        <div className={styles.profileLeft}>
+          <img
+            src="/default-profile-image.jpg"
+            alt="Profile"
+            className={styles.profilePic}
+          />
+        </div>
+        <div className={styles.profileRight}>
+          <h2 className={styles.username}>{myUsername}</h2>
+          <p>{myAlias || "My Alias"}</p>
+          <div className={styles.stats}>
+            <div>30 posts</div>
+            <div>15 followers</div>
+          </div>
+          <div className={styles.bio}>{bio}</div>
+        </div>
+      </div>
+      <div className={styles.profileContentSub}>
+        <p>To be continued area ....</p>
+      </div>
     </div>
   );
 }
