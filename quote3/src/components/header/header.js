68d4cddc338d9db4ed02ec22d53be728c422432b
ZacharyFaria <139006381+ZacharyFaria@users.noreply.github.com>
2024-12-03 22:02:59 -0500

    Fixed Name
    Fixed displayed name to align with the name on the Vision Document

--- quote3/src/components/header/header.js
+++ quote3/src/components/header/header.js
@@ -39,7 +39,7 @@ const Header = () => {
             dispatch(updateUserActions({ isCreatingPost: false }));
           }}
         >
-          Quotes Base
+          Quote Base
         </Link>
       </h2>
       <nav>
